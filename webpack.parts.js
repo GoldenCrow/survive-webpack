e34be76cb3416844da808472fdfee3793c3cccd5
GoldenCrow <peter.stevenson1814@gmail.com>
2017-01-17 08:46:30 +0000

    8. Purify

--- webpack.parts.js
+++ webpack.parts.js
@@ -104,3 +104,27 @@ exports.extractCSS = function (paths) {
     ]
   };
 };
+
+exports.purifyCSS = function(paths) {
+  paths = Array.isArray(paths) ? paths : [paths];
+
+  return {
+    plugins: [
+      new PurifyCSSPlugin({
+        // Our paths are absolute so Purify needs patching
+        // against that to work.
+        basePath: '/',
+        // `paths` is used to point PurifyCSS to files not
+        // visible to Webpack. This expects glob patterns so
+        // we adapt here.
+        paths: paths.map(path => `${path}/*`),
+        purifyOptions: {
+          info: true
+        },
+        // Walk through only html files within node_modules. It
+        // picks up .js files by default!
+        resolveExtensions: ['.html']
+      })
+    ]
+  };
+};
