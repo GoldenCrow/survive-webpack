35bd5160fd2216dbbf08db50c84f7e9c38a4df99
GoldenCrow <peter.stevenson1814@gmail.com>
2017-01-18 22:53:59 +0000

    Analytics

---
+++ stats.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+const config = require('./webpack.config.js')('production');
+
+webpack(config, (err, stats) => {
+  if (err) {
+    return console.error(err);
+  }
+  if(stats.hasErrors()) {
+    return console.error(stats.toString('errors-only'));
+  }
+  console.log(stats);
+});
