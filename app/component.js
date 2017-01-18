ffa814dcd97f4eb6d9803d08e6c99a00cc44e261
GoldenCrow <peter.stevenson1814@gmail.com>
2017-01-18 20:08:53 +0000

    16.1 - Lazy load

--- app/component.js
+++ app/component.js
@@ -1,9 +1,15 @@
-import 'purecss';
 export default function () {
   const element = document.createElement('h1');
 
   element.className = 'pure-button';
   element.innerHTML = 'Hello world';
+  element.onclick = () => {
+    import('./lazy').then((lazy) => {
+      element.textContent = lazy.default;
+    }).catch((err) => {
+      console.error(err);
+    });
+  };
 
   return element;
 }
